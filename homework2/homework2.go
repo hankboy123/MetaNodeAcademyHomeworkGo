60ef96c3f702bb2b849adea9789a01f1ed47edaa
hank <lihaoworking@163.com>
2025-12-25 20:01:32 +0800

    mutiplyBy2

--- homework2/homework2.go
+++ homework2/homework2.go
@@ -3,3 +3,9 @@ package main
 func Add10(pointer *int) {
 	*pointer = *pointer + 10
 }
+
+func mutiplyBy2(pointer *[]int) {
+	for i := range *pointer {
+		(*pointer)[i] = (*pointer)[i] * 2
+	}
+}
--- homework2/main.go
+++ homework2/main.go
@@ -9,4 +9,8 @@ func main() {
 	intPara := 20
 	Add10(&intPara)
 	fmt.Println(intPara)
+
+	slice1 := []int{1, 2, 3, 4, 5}
+	mutiplyBy2(&slice1)
+	fmt.Println("方法1结果:", slice1) // 输出: [2 4 6 8 10]
 }
